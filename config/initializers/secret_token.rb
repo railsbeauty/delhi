88486aba4e8c21ee1c8d6193278bfc9c2340d058
Vinay <vinay59.grewal@gmail.com>
2013-03-24 03:46:39 +0530

    initial comment with bootstrap and devise functionality

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Hr::Application.config.secret_token = 'd1bf506ccfb06a85f4bae552ebf80b53bbd606189b87a0d6a1004182a50bd03e2aaffe7fdeaefdb019c1a3b20b514ed4785cb53550cf9f53e57935e7dad8f3ec'
